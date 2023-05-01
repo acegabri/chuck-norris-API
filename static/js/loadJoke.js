c81c957383f6abc9b4a045b991daf4bbba41aa88
Gabriele Di Cosmo <gabrieledicosmo05@gmail.com>
2023-05-01 19:12:39 +0200

    .

--- static/js/loadJoke.js
+++ static/js/loadJoke.js
@@ -4,6 +4,7 @@ loadJokeButton.addEventListener("click", function (e) {
 	e.preventDefault();
 
 	function thenCallback(response) {
+		console.log("questa è la response del server");
 		console.log(response);
 		console.log(response.status);
 
@@ -18,15 +19,16 @@ loadJokeButton.addEventListener("click", function (e) {
 	}
 
 	function finalCallback(data) {
-		//console.log(data); stampa tutto l'oggetto JSON
-		//console.log(data.value); stampa solamente il joke che è ciò che a noi
+		console.log("questo è l'oggetto JSON restituito");
+		console.log(data); //stampa tutto l'oggetto JSON
+		console.log(data.value); //stampa solamente il joke che è ciò che a noi
 		document.querySelector(".jokeString").innerHTML = data.value;
 
 		// Access an existing link element by ID
 		let myLink = document.querySelector("#jokeLink");
 		// Set the href attribute of the link
 		myLink.href = data.url;
-        myLink.innerHTML = data.url
+		myLink.innerHTML = data.url;
 	}
 
 	fetch("https://api.chucknorris.io/jokes/random")
